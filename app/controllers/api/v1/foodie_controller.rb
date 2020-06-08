a50f0485c51693b7172cbd73a873f14d005ac6c3
Jenny Klich <jennyklich@gmail.com>
2020-06-08 09:34:39 -0600

    Add working on first api call

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::FoodieController < ApplicationController
   def show
-    
+    origin = params[:start]
+    destination = params[:end]
+
+    conn = Faraday.new(url: "https://maps.googleapis.com") do |faraday|
+      faraday.params["key"] = ENV['GOOGLE_MAPS_KEY']
+    end
+
+    response = conn.get("/maps/api/directions/json?origin=#{origin}&destination=#{destination}")
+
   end
 end
