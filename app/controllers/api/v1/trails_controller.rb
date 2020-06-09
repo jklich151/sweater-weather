a54554565f5f01292ce2564efc17cfa241d36d0b
Jenny Klich <jennyklich@gmail.com>
2020-06-09 09:44:32 -0600

    Add work through api calls

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::TrailsController < ApplicationController
   def index
-    
+    location = params[:location]
+    info = GoogleService.new
+    info.coordinates(location)
+    lat_long = info.coordinates(location)[:results][0][:geometry][:location]
+    weather = WeatherService.new.get_current(lat_long)
+    temperature = weather[:current][:temp].to_i
+    summary = weather[:current][:weather][0][:description]
+
+    conn = Faraday.new(url: "http://www.mapquestapi.com") do |faraday|
+      faraday.headers[""] = '<YOUR API KEY>'
+    end
+
+    response = conn.get("/congress/v1/members/house/#{state}/current.json")
+
   end
 end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -6,6 +6,12 @@ class WeatherService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def get_current(results)
+    response = conn.get("/data/2.5/onecall?lat=#{results[:lat]}&lon=#{results[:lng]}&units=imperial")
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def conn
     Faraday.new(url: "https://api.openweathermap.org") do |faraday|
       faraday.params["appid"] = ENV["OPEN_WEATHER_KEY"]
